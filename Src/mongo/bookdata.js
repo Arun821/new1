4f4bb3420b810206525e7d65099f0792c1e8c1f4
Arun PK <61105146+Arun821@users.noreply.github.com>
2020-07-02 11:26:41 +0530

    Update bookdata.js

--- Src/mongo/bookdata.js
+++ Src/mongo/bookdata.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');// Accessing mongoose pacage
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/booklibrary',{ useNewUrlParser: true ,useUnifiedTopology:true});//connection with mongodb
-const Schema = mongoose.Schema;// Defining a schema
+const Schema = mongoose.Schema;
 
 
 const BookSchema = new Schema({
     title : String,
     author : String,
     genre : String,
-    image : String
-})
+    image : String,
+    name : String,
+    discription : String,
+    nationality : String,
+    auimage : String
+});
+
 
 var Bookdata = mongoose.model('book',BookSchema);
+
 
 module.exports = Bookdata;
