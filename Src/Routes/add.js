90325aaf34501efc5a01a514cf7f3ec93d0a8448
Arun PK <61105146+Arun821@users.noreply.github.com>
2020-07-02 11:25:32 +0530

    Update add.js

--- Src/Routes/add.js
+++ Src/Routes/add.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const addRouter = express.Router();
 const Bookdata = require('../mongo/bookdata');
-//const Authordata = require('../mongo/bookdata');
 function add(nav){
     addRouter.get('/',(req,res)=>{
         res.render("add",   
@@ -16,14 +15,17 @@ function add(nav){
             title : req.body.title,
             author : req.body.author,
             genre : req.body.genre,
-            image : req.body.image
+            image : req.body.image,
+            discription : req.body.discription,
+            nationality : req.body.nationality,
+            auimage : req.body.auimage
         }
         var book = Bookdata(newbooks);
         book.save();
         res.redirect('/book');
     });
+     
 
-    //addRouter.post()
     return addRouter;
 }
 
